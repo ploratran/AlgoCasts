1100df15e63ed8f932976b16e582276c7820049b
Plora Tran <ploratran@gmail.com>
2020-10-09 15:46:25 -0700

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - 1 - i];
+    })
+}
 
 module.exports = palindrome;
+
+// return str.split('').reverse().join('') === str ? true : false; 
