6d70d6ca56c486f41b4ce4bf12d52a392fc79bbf
Plora Tran <ploratran@gmail.com>
2020-10-09 15:15:18 -0700

    reverstring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((accumulator, char) => char + accumulator, '');  
+}
 
 module.exports = reverse;
+
+// let reverseStr = '';
+//     for (let char of str) {
+//         /**
+//          * a + "" = "a"
+//          * b + "a" = "ba"
+//          * c + "ba" = "cba"
+//          * d + "cba" = "dcba" 
+//         */
+//         reverseStr = char + reverseStr;
+//     }
+// return reverseStr;
