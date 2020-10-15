928feca18b9e3190f1e6d3ec8f23ba856dbdbf97
Plora Tran <ploratran@gmail.com>
2020-10-14 22:26:26 -0700

    fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,23 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        // multiple of 3 and 5: 
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } 
+        // multiple of 3: 
+        else if (i % 3 === 0) {
+            console.log('fizz');
+        } 
+        // multiple of 5: 
+        else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 module.exports = fizzBuzz;
