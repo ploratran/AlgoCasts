31c95ad665c7da50e4fdf870306b3b38b9194969
Plora Tran <ploratran@gmail.com>
2020-10-18 09:52:18 -0700

    capitalize solution 2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,19 +8,38 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
+    // make a new string with first letter capitalized
+    // iterate over each character in str
+    // if the character to the left is an empty space
+    // then, capitalized the current character
+    // else keep concatenate current character to the new string
+    // return new string
+
+    let result = "" + str.charAt(0).toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i-1] === " ") {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result; 
+}
+
+module.exports = capitalize;
+
     // make an empty array
     // split the string to an array of words
     // iterate thru each character in each word
     // capitalize the first character in each word
     // then concatenate the rest of the character in each word with first character
     // push each word to the empty array and return it
-    const result = [];
+    // const result = [];
 
-    for (let word of str.split(' ')) { 
-        result.push(word.charAt(0).toUpperCase() + word.slice(1));
-    }
+    // for (let word of str.split(' ')) { 
+    //     result.push(word.charAt(0).toUpperCase() + word.slice(1));
+    // }
 
-    return result.join(' '); 
-}
-
-module.exports = capitalize;
+    // return result.join(' '); 
