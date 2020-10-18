d84c1ca0cb7b4e67be4ea2cd2ff6aed592c8c388
Plora Tran <ploratran@gmail.com>
2020-10-17 18:17:48 -0700

    capitalize solution 1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,20 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    // make an empty array
+    // split the string to an array of words
+    // iterate thru each character in each word
+    // capitalize the first character in each word
+    // then concatenate the rest of the character in each word with first character
+    // push each word to the empty array and return it
+    const result = [];
+
+    for (let word of str.split(' ')) { 
+        result.push(word.charAt(0).toUpperCase() + word.slice(1));
+    }
+
+    return result.join(' '); 
+}
 
 module.exports = capitalize;
